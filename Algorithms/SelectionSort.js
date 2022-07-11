9f53de0417cc0606221490adf2afcca86276b5d7
kromate <akpananthony33@gmail.com>
2022-07-11 13:03:38 +0100

    feat: added selection sort algorithm

---
+++ Algorithms/SelectionSort.js
@@ -0,0 +1,29 @@
+const findSmallest = (arr) => {
+	let smallest = arr[0];
+	let smallestIndex = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < smallest) {
+			smallest = arr[i];
+			smallestIndex = i;
+		}
+	}
+
+	return smallestIndex;
+};
+
+const selectionSort = (arr) => {
+	let sortedArr = [];
+	let len = arr.length;
+
+	for (let i = 0; i < len; i++) {
+		let SI = findSmallest(arr);
+		sortedArr.push(arr.splice(SI, 1));
+	}
+
+	return sortedArr.flat();
+};
+
+let sorted = selectionSort([5, 2, 5, 9, 1, 8]);
+
+console.log(sorted);
