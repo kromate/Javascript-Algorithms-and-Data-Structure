07c9d23711a20075011a8c89333744207ec4fc77
Anthony Akpan <akpananthony33@gmail.com>
2022-08-30 13:47:06 +0100

    Time: 128 ms (82.89%), Space: 48.8 MB (75.94%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -13,24 +13,24 @@
     - get the second elem of the result array and store it with a temp val
     - set the new second elem to the max between end and temp val
 - else push the whole array to result
-
+- return result
 */
-
-var merge = function (intervals) {
-  // Sort the intervals
-  intervals.sort((a, b) => a[0] - b[0]);
-  const res = [intervals[0]];
-  for (let i = 1; i < intervals.length; i++) {
-    const [start, end] = intervals[i];
-    // If previous interval ends after current item start, then merge
-    if (res[res.length - 1][1] >= start) {
-      const lastEnd = res[res.length - 1][1];
-      // Update the ending time of new interval as the maximum of last interval's end and current interval end
-      res[res.length - 1][1] = Math.max(lastEnd, end);
-    } else {
-      // If previous interval ends before current item start, then simply add this interval in result array.
-      res.push([start, end]);
+var merge = function(intervals) {
+    intervals.sort((a,b)=> a[0]-b[0])
+    const result = [intervals[0]]
+    
+    for(let i=1; i<intervals.length; i++){
+      const [start, end] = intervals[i]
+      
+      if(result[result.length-1][1] >= start){
+          lastEnd = result[result.length-1][1]
+          
+          result[result.length-1][1] = Math.max(lastEnd, end)
+      }else{
+          result.push(intervals[i])
+      }
+        
     }
-  }
-  return res;
+    
+    return result
 };
